27daf937f54f2cb569c279ee9d68c49f19498816
Sebastian C <seb.czech@wp.pl>
2020-06-19 18:43:14 +0200

    Prepare jar to heroku deploy

--- src/main/java/pl/seb/czech/ilegal/front/ui/view/HomeView.java
+++ src/main/java/pl/seb/czech/ilegal/front/ui/view/HomeView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import pl.seb.czech.ilegal.front.backend.clients.ChangeViewClient;
 import pl.seb.czech.ilegal.front.domain.ChangeViewLog;
 import pl.seb.czech.ilegal.front.ui.layout.MainLayout;
@@ -22,27 +23,45 @@ public class HomeView extends VerticalLayout {
         changeViewClient.save(new ChangeViewLog(this.getClass().getSimpleName()));
         
         Label content = new Label();
-        String welcomeMessage = "<h1>Witaj w aplikacji I-Legal</h1>";
-        try {
-            welcomeMessage = getWelcomeContent();
-        } catch (IOException e) {
-            log.error("Error while reading Home.html file", e);
-        } 
-        content.getElement().setProperty("innerHTML", welcomeMessage);
+        content.getElement().setProperty("innerHTML", body);
         add(content);
         
     }
     
-    public String getWelcomeContent() throws IOException {
-        File html = new File(getClass().getClassLoader().getResource("templates/Home.html").getPath());
-        BufferedReader br = new BufferedReader(new FileReader(html));
-        
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while((line = br.readLine()) != null) {
-            sb.append(line);
-        }
-        return sb.toString();
-        
-    }
+    private final String body = "<!DOCTYPE html>\n" +
+            "<html lang=\"en\">\n" +
+            "<head>\n" +
+            "    <meta charset=\"UTF-8\">\n" +
+            "    <title>Home</title>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "    <h1>Witaj w aplikacji I-Legal</h1>\n" +
+            "    <div>\n" +
+            "        Aplikacja składa się z 2 powiązanych ze sobą modułów:\n" +
+            "    </div>\n" +
+            "    <ul>\n" +
+            "        <li>Aktów prawnych</li>\n" +
+            "        <li>Orzeczeń sądów i innych organów</li>\n" +
+            "    </ul>\n" +
+            "    <div>\n" +
+            "        Każdy z modułów posiada wyszukiwarkę, która za pośrednictwem publicznych API przeszukuje zbiory odpowiednio:\n" +
+            "    </div>\n" +
+            "    <ul>\n" +
+            "        <li><a href=\"http://isap.sejm.gov.pl/api/isap/\">Internetowego System Aktów Prawnych (ISAP)</a> oraz</li>\n" +
+            "        <li><a href=\"https://www.saos.org.pl/help/index.php/dokumentacja-api\">Analizy Orzeczeń Sądowych (SAOS)</a></li>\n" +
+            "    </ul>\n" +
+            "    <div>\n" +
+            "        Wyniki wyszukiwań (tj. akty prawne i orzeczenia) oraz ich szczegóły mogą być zapisywane w bazie danych. Ponadto w panelu szczegółów \n" +
+            "        (po kliknięciu w akt lub orzeczenie) można automatycznie przenieść się do uzupełnionego formularza wyszukiwań \n" +
+            "        celem znalezienia powiązanych elementów. \n" +
+            "    </div>\n" +
+            "    <div>\n" +
+            "        W przypadku zapisanych aktów prawnych codziennie automatycznie sprawdzana ich aktualność (można to również \n" +
+            "        zrobić ręcznie w zakładce zapisane akty prawne). \n" +
+            "    </div>\n" +
+            "    <div>\n" +
+            "        <p>Autor: Sebastian Czech\n" +
+            "    </div>\n" +
+            "</body>\n" +
+            "</html>";
 }
